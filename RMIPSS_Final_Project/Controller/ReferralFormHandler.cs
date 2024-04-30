98de89aa7ff68fe26ee5dd3e633824d27925568f
Amit Maharjan <maharjanamit8@gmail.com>
2024-04-30 10:19:02 -0400

    Initial Project Added

---
+++ RMIPSS_Final_Project/Controller/ReferralFormHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMIPSS_Final_Project.Controller
+{
+    internal class ReferralFormHandler
+    {
+    }
+}
---
+++ RMIPSS_Final_Project/Model/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMIPSS_Final_Project.Model
+{
+    internal class Student
+    {
+        private String lastName;
+        private String firstName;
+        private String middleName;
+
+        private String village;
+        private String atoll;
+        private String poBoxNo;
+
+
+    }
+}
